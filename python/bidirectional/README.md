45697df194c6cacf16c7c131af99e6c39363eb0a
duykha0205 <duykha0205@gmail.com>
2023-07-24 16:19:47 +0700

    gRPC-bidirectional: py_server <-> py_client

---
+++ python/bidirectional/README.md
@@ -0,0 +1,8 @@
+### REF
+
+https://www.velotio.com/engineering-blog/grpc-im    plementation-using-python
+
+https://www.youtube.com/watch?v=8QRhlPnOKhM&ab_channel=yingshaoxo%27slab
+
+https://www.youtube.com/watch?v=WB37L7PjI5k 
+
---
+++ python/bidirectional/__init__.py
@@ -0,0 +1,6 @@
+# Import the modules from the package
+import bidirectional_pb2_grpc as bidirectional_pb2_grpc
+import bidirectional_pb2 as bidirectional_pb2
+
+# Optionally, you can provide a convenient way to access the modules by using __all__
+__all__ = ['bidirectional_pb2_grpc']
